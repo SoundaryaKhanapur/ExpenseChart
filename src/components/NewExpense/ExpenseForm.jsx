b99c93208e8e4500313da0e5873d23c39b54ae9b
Soundarya Khanapur <soundarya.m.khanapur@gmail.com>
2021-12-05 08:40:15 +0530

    2 way binding to form

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -70,6 +70,11 @@ const ExpenseForm = () => {
         };
 
         console.log(expenseData)
+        setUserInput({
+            enteredTitle: '',
+            enteredAmount: '',
+            enteredDate: ''  
+        })
     }
 
     return(
@@ -77,15 +82,26 @@ const ExpenseForm = () => {
           <div className="new-expense__controls">
               <div className="new-expense__control">
                   <label>Title</label>
-                  <input type='text' onChange={titleChangeHandler}/>
+                  <input type='text'
+                  value={userInput.enteredTitle}
+                  onChange={titleChangeHandler}/>
               </div>
               <div className="new-expense__control">
                   <label>Amount</label>
-                  <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                  <input type='number'
+                   min="0.01"
+                    step="0.01"
+                    value={userInput.enteredAmount}
+                    onChange={amountChangeHandler}/>
               </div>
               <div className="new-expense__control">
                   <label>Date</label>
-                  <input type='date' min="2020-01-01" max="2023-12-31" onChange={dateChangeHandler} />
+                  <input
+                   type='date'
+                   min="2020-01-01" 
+                   max="2023-12-31" 
+                   value={userInput.enteredDate}
+                   onChange={dateChangeHandler} />
               </div>
               <div className="new-expense__actions">
                   <button type='submit'>Add Expense</button>
